c43d33b2c83f7a77e0b5559c48aecfda7ab63e15
alvaro <alvarodevcode@outlook.com>
2023-09-06 22:22:32 -0500

    fix: build fix

---
+++ src/app/auth/onboarding/page.tsx
@@ -0,0 +1,10 @@
+export default function OnboardingPage() {
+  return (
+    <section>
+      <h2>Welcome aboard</h2>
+
+      <p>Let&apos;s register your company</p>
+      <span className="underline">I already have one</span>
+    </section>
+  );
+}
